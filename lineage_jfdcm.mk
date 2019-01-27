67ee82d53b4f700e0a318511a47d070bba383753
yutotetuota <34832037+yutotetuota@users.noreply.github.com>
2019-01-26 23:30:07 -0800

    Update lineage_jfdcm.mk

--- lineage_jfdcm.mk
+++ lineage_jfdcm.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from jfltexx device
-$(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
+$(call inherit-product, device/samsung/jfdcm/full_jfdcm.mk)
 
 PRODUCT_NAME := lineage_jfdcm
 PRODUCT_DEVICE := jfdcm
