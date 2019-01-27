77e9b1e849b3babb6421b64e421a2af55eba7376
yutotetuota <34832037+yutotetuota@users.noreply.github.com>
2019-01-26 19:35:37 -0800

    Update and rename full_jfltexx.mk to full_jfdcm.mk

--- full_jfdcm.mk
+++ full_jfdcm.mk
@@ -25,11 +25,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from jfltexx device
-$(call inherit-product, device/samsung/jfltexx/device.mk)
+$(call inherit-product, device/samsung/jfdcm/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_jfltexx
-PRODUCT_DEVICE := jfltexx
+PRODUCT_NAME := full_jfdcm
+PRODUCT_DEVICE := jfdcm
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := GT-I9505
+PRODUCT_MODEL := SC-04E
